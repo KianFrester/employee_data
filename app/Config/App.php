b99ca4ddb935fddd013e11f86b1610ae7e616092
KianFrester <kianmaximo17@gmail.com>
2026-01-27 11:03:14 +0800

    changed base_url

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,8 +17,8 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
 
-    // public string $baseURL = 'http://localhost/ci4/ci4/public/';
-    public string $baseURL = 'http://192.168.1.250/ci4/ci4/public/';
+    public string $baseURL = 'http://localhost/ci4/ci4/public/';
+    // public string $baseURL = 'http://192.168.1.250/ci4/ci4/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
